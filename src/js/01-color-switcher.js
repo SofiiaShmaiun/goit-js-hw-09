40f9fa77bf009f9bcd632d35f115880bfd7e9fb4
Sofiia Shmaiun <sofiia.shmaiun@gmail.com>
2023-03-16 19:23:24 +0200

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+let intervalId;
+const bodySheet = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+startBtn.addEventListener('click', handleStartColorChanging);
+stopBtn.addEventListener('click', handleStopColorChanging);
+
+function handleStartColorChanging() {
+  intervalId = setInterval(() => {
+    bodySheet.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+}
+
+function handleStopColorChanging() {
+  clearInterval(intervalId);
+  startBtn.disabled = false;
+}
